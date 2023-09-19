385ac87cab18e5ebec7eaa504bb934fe39163c1d
BRUNO DA SILVA TEIXEIRA <132865117+brunojureia@users.noreply.github.com>
2023-09-19 02:24:48 -0300

    atualizei

---
+++ variaveiscompostasefunçoes/embiente.js
@@ -0,0 +1,24 @@
+ let num = [5,8,2,9,3]
+ num.sort()
+ num.push(1)
+console.log(num)
+console.log(`o vetor tem  ${num.length} posiçoes `)
+console.log (`o primeiro valor do vetor e ${num[0]}`)
+let pos = num.indexOf (8)
+if(pos == -1 ){
+console.log(`o valor nao foi encontrado`)
+
+}else{
+    console.log(`o valor esta na posiçao ${pos}`)
+}
+
+
+
+
+
+
+
+
+ // num[3]=6  bare mais uma variaveis
+// num.push(7) 
+ //num.length mede quandas variaveis tem {0.1.3.4. }
